b253493d912fb321a236c8dac323df0fde0c95d3
Lumenkowski <lumenkowski@gmail.com>
2024-06-09 09:17:42 +0500

    Create `Logger` module

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log( "TypeScript Codebase" )
+import { Logger, LogLevel } from "./library/Logger"
+
+const logger = new Logger( LogLevel.DEBUG, {
+    [ LogLevel.DEBUG ]: { color: "white", backgroundColor: "black", padding: "2px 8px", borderRadius: "2px" },
+    [ LogLevel.INFO ]: { color: "black", backgroundColor: "lightblue", padding: "2px 8px", borderRadius: "2px" },
+    [ LogLevel.WARN ]: { color: "black", backgroundColor: "yellow", padding: "2px 8px", borderRadius: "2px" },
+    [ LogLevel.ERROR ]: { color: "black", backgroundColor: "red", padding: "2px 8px", borderRadius: "2px" }
+} )
+
+logger.debug( "This is a debug message." )
+logger.info( "This is an info message." )
+logger.warn( "This is a warning message." )
+logger.error( "This is an error message." )
